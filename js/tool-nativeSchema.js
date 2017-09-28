8d36c3fa81af4a883e0923ddc02dc1b53e5bffd5
chenzehua <815054699@qq.com>
2017-09-28 16:15:05 +0800

    '测试另一种打开APP的测试'

--- js/tool-nativeSchema.js
+++ js/tool-nativeSchema.js
@@ -2,7 +2,7 @@
  * @author alanzhang
  * @date 2016-07-15
  * @overview 测试在浏览器中打开native App;
- * nativeSchema.loadSchema({});
+ *
  */
 
 (function(root, factory){
@@ -40,8 +40,8 @@
         PROTOCAL:"HMClient",
 
         // 主页
-        HOME: "openapp",
-
+        HOME: "",
+        
         // 唤起失败时的跳转链接
         FAILBACK: {
             ANDROID: "https://a.app.qq.com/o/simple.jsp?pkgname=com.jlhm.personal",
@@ -54,7 +54,8 @@
             CATEGORY: "android.intent.category.DEFAULT",
             ACTION: "android.intent.action.VIEW"
         },
-
+        //是否跳转下载
+        DOWN_LOAD_PAGE:true,
         // 唤起超时时间，超时则跳转到下载页面
         LOAD_WAITING: 3000
     };
@@ -64,9 +65,8 @@
     // 是否为Android下的chrome浏览器，排除mobileQQ；
     // Android下的chrome，需要通过特殊的intent 来唤醒
     // refer link：https://developer.chrome.com/multidevice/android/intents
-    var isAndroidChrome = (ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/))
+    var isAndroidChrome = (ua.match(/Chrome\/([\d.]+)/) || ua.match(/CriOS\/([\d.]+)/) || ua.match(/Lenovo/ig))
                           && browser.isAndroid() && !browser.isMobileQQ();
-
     return {
         /**
          * [mixinConfig 重新收拢配置]
@@ -89,6 +89,9 @@
                 AppConfig.APK_INFO = config.apkInfo || AppConfig.APK_INFO;
             }
             
+            if(typeof config.DOWN_LOAD_PAGE == 'boolean'){
+            	AppConfig.DOWN_LOAD_PAGE = config.DOWN_LOAD_PAGE ;	
+            }
         },
         /**
          * [generateSchema 根据不同的场景及UA生成最终应用的schema]
@@ -98,7 +101,7 @@
 
             var localUrl  = window.location.href;
             var schemaStr = '';
-			
+
             // 如果未定义schema，则根据当前路径来映射
             if (!schema) {
                 schemaStr = AppConfig.HOME;
@@ -138,12 +141,15 @@
 
             // 隐藏iframe及a
             aLink.style.cssText = iframe.style.cssText = "display:none;width:0px;height:0px;";
+
             // Android 微信不支持schema唤醒，必须提前加入腾讯的白名单
             if (browser.isWx() && browser.isAndroid()) {
+
                 window.location.href = AppConfig.FAILBACK.ANDROID;
 
             // ios 9 safari 不支持iframe的方式跳转
             } else if (browser.isIOS()) {
+
                 if (browser.isWx()) {
                     window.location.href = AppConfig.FAILBACK.IOS;
                 } else {
@@ -151,10 +157,10 @@
                     body.appendChild(aLink);
                     aLink.click();
                 }
+
             // Android chrome 不支持iframe 方式唤醒
-            // 适用：chrome,leibao,mibrowser,opera,360
+            // 适用：chrome,leibao,mibrowser,opera,360 
             } else if (isAndroidChrome) {
-//          	window.location.href = AppConfig.FAILBACK.ANDROID;
                 aLink.href = schemaUrl;
                 body.appendChild(aLink);
                 aLink.click();
@@ -164,28 +170,33 @@
             } else {
                 body.appendChild(iframe);
                 iframe.src = schemaUrl;
+
             }
-			
+
             // 如果LOAD_WAITING时间后,还是无法唤醒app，则直接打开下载页
             // opera 无效
             var start = Date.now(),
                 that  = this;
-            loadTimer = setTimeout(function() {				
+            loadTimer = setTimeout(function() {
+
                 if (document.hidden || document.webkitHidden) {
                     return;
                 }
-
+                /*console.log(不跳转下载页面);*/
+				if(!AppConfig.DOWN_LOAD_PAGE){return false;}
+				
                 // 如果app启动，浏览器最小化进入后台，则计时器存在推迟或者变慢的问题
                 // 那么代码执行到此处时，时间间隔必然大于设置的定时时间
                 if (Date.now() - start > AppConfig.LOAD_WAITING + 200) {
                     // come back from app
-                    
+
                     // 如果浏览器未因为app启动进入后台，则定时器会准时执行，故应该跳转到下载页
                 } else {
                     window.location.href = browser.isIOS() ? AppConfig.FAILBACK.IOS : AppConfig.FAILBACK.ANDROID;
                 }
-
+                
             }, AppConfig.LOAD_WAITING);
+
 
             // 当本地app被唤起，则页面会隐藏掉，就会触发pagehide与visibilitychange事件
             // 在部分浏览器中可行，网上提供方案，作hack处理
